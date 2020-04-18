8ecfaa2daa24980dd28ca852d2272292a2cce171
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-19 01:08:19 +0530

    Search functionality for API Added

--- rest/views.py
+++ rest/views.py
@@ -11,6 +11,14 @@ class TweetViewSet(viewsets.ModelViewSet):
     serializer_class = TweetSerializer
     queryset = Tweet.objects.all().order_by('-added')
 
+    def get_queryset(self):
+        request = self.request
+        qs = Tweet.objects.all()
+        query = request.GET.get('search')
+        if query is not None:
+            qs = qs.filter(title__icontains=query)
+        return qs
+
 
 def test(request):
     return render(request, 'index.html')
